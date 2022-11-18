c5d7df302cfb161202e718408ced6803cf94251e
Sizaya <sizzzaya@gmail.com>
2022-11-18 14:01:26 +0200

    Commit 5. Lesson 10. DZ 3

---
+++ Lesson_10/DZ_10_3.py
@@ -0,0 +1,16 @@
+class MyError(Exception):
+   def __init__(self, text):
+      self.text = text
+
+a = input("Input positive number: ")
+try:
+   a = int(a)
+   if a < 0:
+      raise MyError("You give negative!")
+except ValueError:
+   print(" Error type of value!")
+except MyError as mr:
+   print(mr)
+else:
+   print(a)
+
